6dc1a31b4c3bf7af1a44ac63b346b7c04a3a046c
hkardouni <h.kardouni@gmail.com>
2024-05-21 13:29:10 +0330

    create pagesand routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
-export default function App() {
+const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
