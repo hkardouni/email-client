037d48b8bc34cd4c75bc8260a3e4317c5ec286ff
hkardouni <h.kardouni@gmail.com>
2024-05-25 14:48:41 +0330

    create signup API route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL).then(() => {
@@ -11,7 +12,10 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
         console.log(`Connection failed with this error: ${e}`);
     })
 const app = express()
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000...');
 })
+
+app.use('/api/auth', authRouter)
